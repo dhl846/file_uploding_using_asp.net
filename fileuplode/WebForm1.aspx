b22abe4abea9c13608a27a90f13cdac2cb55b126
dhl846 <laddhruvin84@gmail.com>
2020-09-30 12:11:43 +0530

    will help us uplod file using web page

---
+++ fileuplode/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="fileuplode.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            select a file to be uploded
+            <br/>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br/>
+            <asp:Button ID="Btnuplode" runat="server" Text="uplode" OnClick="Btnuplode_Click" />
+            <asp:Label ID="lbluplode" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ fileuplode/WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace fileuplode
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btnuplode_Click(object sender, EventArgs e)
+        {
+            string filename, filepath, fileext;
+            filename = "";
+            filepath = "dhruvin/";
+            fileext = "";
+            if (FileUpload1.HasFile)
+            {
+                filename = FileUpload1.FileName;
+                fileext = Path.GetExtension(filename);
+                if ((fileext == ".jpg") || (fileext == ".gif") || (fileext == ".png"))
+                {
+                    FileUpload1.SaveAs(Server.MapPath(filepath) + filename);
+                    lbluplode.Text = "image is uploded succesfully your file size is:" + FileUpload1.FileBytes.Length.ToString();
+                }
+                else
+                {
+                    lbluplode.Text = "you dont have a correct file formate";
+                }
+            }
+            else
+            {
+                lbluplode.Text = "file uploded success!!";
+            }
+            //Console.ReadKey();
+        }
+    }
+}
